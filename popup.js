aa45ab74c24e887bc9ecaf934e2505f9e69ed996
ddayal-fiksu <ddayal@fiksu.com>
2014-09-03 17:59:26 -0400

    Init

---
+++ popup.js
@@ -0,0 +1,126 @@
+/* U+200B below */
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+/**
+ * Global variable containing the query we'd like to pass to Flickr. In this
+ * case, kittens!
+ *
+ * @type {string}
+ */
+var QUERY = 'kittens';
+var RECIPIENT = 'devend711@gmail.com'
+
+var kittenGenerator = {
+  /**
+   * Flickr URL that will give us lots and lots of whatever we're looking for.
+   *
+   * See http://www.flickr.com/services/api/flickr.photos.search.html for
+   * details about the construction of this URL.
+   *
+   * @type {string}
+   * @private
+   */
+  searchOnFlickr_: 'https://secure.flickr.com/services/rest/?' +
+      'method=flickr.photos.search&' +
+      'api_key=90485e931f687a9b9c2a66bf58a3861a&' +
+      'text=' + encodeURIComponent(QUERY) + '&' +
+      'safe_search=1&' +
+      'content_type=1&' +
+      'sort=interestingness-desc&' +
+      'per_page=20',
+
+  /**
+   * Sends an XHR GET request to grab photos of lots and lots of kittens. The
+   * XHR's 'onload' event is hooks up to the 'showPhotos_' method.
+   *
+   * @public
+   */
+  requestKittens: function() {
+    var req = new XMLHttpRequest();
+    req.open("GET", this.searchOnFlickr_, true);
+    req.onload = this.showPhotos_.bind(this);
+    req.send(null);
+  },
+
+  /**
+   * Handle the 'onload' event of our kitten XHR request, generated in
+   * 'requestKittens', by generating 'img' elements, and stuffing them into
+   * the document for display.
+   *
+   * @param {ProgressEvent} e The XHR ProgressEvent.
+   * @private
+   */
+  showPhotos_: function (e) {
+    var kittens = e.target.responseXML.querySelectorAll('photo');
+    for (var i = 0; i < kittens.length; i++) {
+      var img = document.createElement('img');
+      img.src = this.constructKittenURL_(kittens[i]);
+      img.setAttribute('alt', kittens[i].getAttribute('title'));
+      document.body.appendChild(img);
+    }
+  },
+
+  /**
+   * Given a photo, construct a URL using the method outlined at
+   * http://www.flickr.com/services/api/misc.urlKittenl
+   *
+   * @param {DOMElement} A kitten.
+   * @return {string} The kitten's URL.
+   * @private
+   */
+  constructKittenURL_: function (photo) {
+    return "http://farm" + photo.getAttribute("farm") +
+        ".static.flickr.com/" + photo.getAttribute("server") +
+        "/" + photo.getAttribute("id") +
+        "_" + photo.getAttribute("secret") +
+        "_s.jpg";
+  },
+
+  htmlForEmail: function (){
+
+  },
+
+  sendEmail: function() { 
+    console.log('starting email send');
+    var tabUrl;
+    chrome.tabs.getSelected(null, function(tab) {
+      tabUrl = tab.url;
+      console.log(tabUrl);
+    });
+    $.ajax({
+    type: 'POST',
+    url: 'https://mandrillapp.com/api/1.0/messages/send.json',
+    data: {
+      'key': 'ZzTcuFPhdBA6ohwJUVR27A',
+      'message': {
+        'from_email': 'devend711@gmail.com',
+        'to': [
+            {
+              'email': RECIPIENT,
+              'name': 'the recipients name',
+              'type': 'to'
+            }
+          ],
+        'autotext': 'true',
+        'subject': tabUrl,
+        'html': 'YOUR EMAIL CONTENT HERE! YOU CAN USE HTML!'
+      }
+    },
+    success: function(result) {
+      console.log('send succeeded!');
+    },
+    failure: function(result) {
+      console.log('send failed!');
+    }
+   });
+  }
+};
+
+// Run our kitten generation script as soon as the document's DOM is ready.
+document.addEventListener('DOMContentLoaded', function () {
+  console.log('hi');
+  kittenGenerator.requestKittens();
+  kittenGenerator.sendEmail();
+});
